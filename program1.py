8d223da7e64740cb43d386c09086945be0eed37c
Harsh <aparmarharsh@gmail.com>
2024-04-25 14:00:40 +0530

    all test cases passed

--- program1.py
+++ program1.py
@@ -1,13 +1,20 @@
+
 class Solution(object):
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        pass
-    
-
-
-
-  
-
+        stack = []
+        
+        mappings = {")": "(", "}": "{", "]": "["}
+        
+        for char in s:
+            if char in mappings:
+                top_element = stack.pop() if stack else '#'
+                
+                if mappings[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
--- program2.py
+++ program2.py
@@ -1,7 +1,29 @@
+
 class Solution(object):
     def romanToInt(self, s):
         """
         :type s: str
         :rtype: int
         """
-        pass
+        
+        roman_numerals = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        result = 0
+        
+        for i in range(len(s)):
+            
+            if i < len(s) - 1 and roman_numerals[s[i]] < roman_numerals[s[i + 1]]:
+                result -= roman_numerals[s[i]]
+            
+            else:
+                result += roman_numerals[s[i]]
+        
+        return result
